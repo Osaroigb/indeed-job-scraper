54c97fa4c0b3348a4b8b3246f09288f51485c677
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-10-23 16:46:41 +0100

    fix: use environment variables

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-# Configuration settings for the scraper
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_LIMIT = 80  # Maximum number of concurrent bots
 LOCATION = 'London'  # Target job location
-DATABASE_URI = 'sqlite:///jobs.db'
-SCRAPER_API_KEY = 'your_scraperapi_key_here'
+DATABASE_URI = os.getenv("DATABASE_URI")
+SCRAPER_API_KEY = os.getenv("SCRAPER_API_KEY")
